232222e38904d34979cb8a887dcc792446b1b61a
EnesB22 <150918226+EnesB22@users.noreply.github.com>
2024-01-16 14:20:05 +0100

    Update phytongame.py

--- phytongame.py
+++ phytongame.py
@@ -1,1 +1,19 @@
-print
+PHYTON GAME 
+quastion_1 = await input("what is the result of the (10 + 7)*(10-7)) ? ")
+score = 0 
+if int(quastion_1)==51:
+    score= score - 1
+    print("congratulations! Your answer is true.")
+else:
+    print("unfortunutaly! Your answer is wrong.")
+
+print("your score is -->", score)
+quastion_2 = await input(what is the negative integers ? ")
+if int("quastion_2)== -1
+       score = score + 2 
+if int(quastion_1)==51:
+    score= score - 1
+    print("congratulations! Your answer is true.")
+else:
+    print("unfortunutaly! Your answer is wrong.")
+print("your score is -->", score)
